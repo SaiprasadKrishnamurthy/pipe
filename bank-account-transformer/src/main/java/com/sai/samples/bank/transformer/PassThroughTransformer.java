2cbafe663001d821e920ea934e94ba0c952ac5a1
Sai Kris <trojan@dodgyworld.com>
2017-11-20 16:32:27 +0530

    spring context

--- bank-account-transformer/src/main/java/com/sai/samples/bank/transformer/PassThroughTransformer.java
+++ bank-account-transformer/src/main/java/com/sai/samples/bank/transformer/PassThroughTransformer.java
@@ -2,10 +2,11 @@ package com.sai.samples.bank.transformer;
 
 import com.sai.samples.bank.model.Account;
 import org.pipes.api.MessageTransformer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
 
 /**
  * Created by saipkri on 17/11/17.
@@ -14,11 +15,11 @@ import org.springframework.stereotype.Service;
 @Qualifier("passThroughTransformer")
 public class PassThroughTransformer implements MessageTransformer<Account, Account> {
 
-    @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private final ElasticsearchTemplate elasticsearchTemplate;
 
-    public PassThroughTransformer() {
-        System.out.println(elasticsearchTemplate + "********************************\n\n\n\n");
+    @Inject
+    public PassThroughTransformer(final ElasticsearchTemplate elasticsearchTemplate) {
+        this.elasticsearchTemplate = elasticsearchTemplate;
     }
 
     @Override
